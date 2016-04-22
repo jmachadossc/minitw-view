96429ec6fb3ff801b86fe1484afd7838e3cfd1be
Juan Manuel <jmachado@securityscorecard.io>
2016-04-22 17:42:05 -0300

    Path redirectioning

--- app/controllers.js
+++ app/controllers.js
@@ -1,17 +1,19 @@
 var minitw = angular.module('minitw');
 
-minitw.controller('UserCtrl', function ($scope, $http) {
+minitw.controller('UserCtrl', function ($scope, $http, $location) {
 	var serverurl = 'http://localhost:7000/users/';
 	$scope.login = function(){
 		// console.log("$scope.uemail",$scope.uemail);
 		$http.get(serverurl + $scope.uemail)
 		.success(function(data) {
-    		$scope.user = data; 
+    		$scope.user = data;
+    		if($scope.user){
+  				$location.path('/main');
+  			} 
     	});
+    	
   	};
-  	if($scope.user){
-  		// $ngRoute.redirectTo('/login');
-  	}
+  	
 });
 
 
