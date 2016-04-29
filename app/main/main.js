89bf2362071a41ba0b671f0a7c07fcaa473953a0
Juan Manuel <jmachado@securityscorecard.io>
2016-04-29 17:54:45 -0300

    idk

--- app/main/main.js
+++ app/main/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
 angular.module('minitw')
 
 // .config(['$routeProvider', function($routeProvider) {
@@ -9,54 +11,84 @@ angular.module('minitw')
 //   }).otherwise({redirectTo: '/login'});
 // }])
 
-.controller('MainCtrl', function($scope, $http, $location, $rootScope ) {
-	var serverurl = 'http://localhost:7000/tweets/';
-	$scope.getTweets = function(){
-		$http.get(serverurl)
-		.success(function (response) {
-    		$scope.tweets = response;
-    		if($rootScope.user){
-  				$location.path('/main');
-  			} 
-    });
+.controller('MainCtrl',['$scope','$location','$http', '$rootScope', 'FollowService',
+ function ($scope, $location, $http, $rootScope, FollowService) {
+  if($rootScope.user){
+    var serverurl = 'http://localhost:7000/users/';
     
-  };
-	$scope.sendTweet = function(){
-		if($scope.user){
-  		var newTweet = {
-  			content: $scope.newTweet,
-  			submitter: $scope.user.email
-  		};  			
-		}else{
-			return console.error('Undefined user');
-		}
-
-		if(newTweet.content && newTweet.submitter){
-  		$http.post(serverurl, newTweet)
-  		.success(function (response){
-  			$scope.getTweets();
-  		});
-		}
-	}; 
-	$scope.getTweets();
-	if(!$rootScope.user){
-		$location.path('/login');
-	}
-  $scope.deleteTweet = function(tweet){
-    if(tweet._id){
-      if($scope.user.email===tweet.submitter){
-        $http.delete(serverurl + tweet._id )
-        .success(function (response){
-          $scope.getTweets();
-        });  
-      }else{
-        $scope.error='Cannot delete that'
+    $scope.getTweets = function(){
+      var config = {
+        method: 'GET',
+        url: serverurl + $rootScope.user.email +'/tweets/'
       }
-        
-    }else{
-      return console.error('Tweet is undefined'); 
-    }
+      $http(config)
+      .success(function (response) {
+        console.log(response , 'this should display tweets');
+        if(response){
+          $scope.tweets = response;
+        }
+      });
+    };   
+      
+       
+    
+      
       
     
-  }
-});
+    $scope.sendTweet = function(){
+      if($scope.user){
+        var newTweet = {
+          content: $scope.newTweet,
+          submitter: $scope.user.email
+        };        
+      }else{
+        return console.error('Undefined user');
+      }
+
+      if(newTweet.content && newTweet.submitter){
+        $http.post(serverurl, newTweet)
+        .success(function (response){
+          $scope.getTweets();
+        });
+      }
+    }; 
+    
+    
+    $scope.deleteTweet = function(tweet){
+      if(tweet._id){
+        if($scope.user.email===tweet.submitter){
+          $http.delete(serverurl + tweet._id )
+          .success(function (response){
+            $scope.getTweets();
+          });  
+        }else{
+          $scope.error='Cannot delete that'
+        }
+          
+      }else{
+        return console.error('Tweet is undefined'); 
+      }    
+    }
+
+    function getFollowingCb(response){
+
+      if(response){
+        $scope.following = response;
+        $scope.getTweets();
+      }else{
+        $scope.getTweets();
+        $scope.error = 'Not following anyone';
+      }
+    }
+
+    function refreshFollowers(){
+      FollowService.getFollowing($rootScope.user, getFollowingCb);
+    }
+    refreshFollowers();
+      
+  }else{
+    $location.path('/login');
+  } 
+	
+
+}]);
--- app/profile/profile.js
+++ app/profile/profile.js
@@ -2,74 +2,30 @@
 
 angular.module('minitw')
 
-.controller('ProfileCtrl', function ($scope, $http, $location, $rootScope ) {
-	if(!$rootScope.user){
-		$location.path('/login');
-	}
-
-
-	var serverurl = 'http://localhost:7000/follow';
-	
-
-
-	$scope.getFollowing = function (){
+.controller('ProfileCtrl',['$scope','$location', '$rootScope', 'FollowService',
+	function ($scope, $location, $rootScope, FollowService) {
 		if($rootScope.user){
-			$http.get(serverurl+'/following/'+$rootScope.user.email)
-			.success(function (response){
-				$scope.following = response;
-			});			
+			function getFollowingCb(data){
+				if(!data){
+					$location.path('/login');
+				}else{
+					$scope.following = data;
+				}
+			}
+
+			function refresh(){
+				FollowService.getFollowing($rootScope.user, getFollowingCb);
+			}	
+
+			$scope.follow = function(){
+				FollowService.follow($rootScope.user, $scope.emailToFollow,refresh);
+			}	
+			refresh();
+			
 		}else{
 			$location.path('/login');
 		}
 
-	}
-	
-	$scope.getFollowing();
-
-	$scope.follow = function (){
-		var relation = {
-			followerEmail:$rootScope.user.email,
-			followedEmail:$scope.emailToFollow
-		}
-
-		$http.get('http://localhost:7000/users/'+$scope.emailToFollow)
-		.then(function (response){
-			$http.post(serverurl, relation)
-			.success(function (response){
-				$scope.message = response;
-				$scope.getFollowing();
-			});	
-		}, function (response){
-			$scope.error = 'User not found'
-		});
 		
+}]);
 
-	}
-
-	$scope.unfollow = function (emailToUnfollow){
-		// var newReq = {
-		// 	method:'DELETE',
-		// 	url:serverurl,
-		// 	data: {
-		// 		followerEmail:victim.followerEmail,
-		// 		followedEmail:victim.followedEmail
-		// 	}
-		// }
-		var victim = {
-			followedEmail:emailToUnfollow,
-			followerEmail:$rootScope.user.email
-		}
-		$http.put(serverurl, victim)
-		.then(function (response){
-			$scope.getFollowing();
-		}, function (response){
-			console.error(response.data);
-		});
-	}
-
-	$scope.getFollowers = function (){
-
-	};
-
-		
-});
